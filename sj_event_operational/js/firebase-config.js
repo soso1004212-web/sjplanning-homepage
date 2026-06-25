c43bddf15aeadfa88f7cc6f3c2b51f8a043e956e
soso1004212-web <soso1004212@gmail.com>
2026-06-25 11:05:18 +0900

    Add files via upload

---
+++ sj_event_operational/js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase 설정 파일
+// 1) Firebase 콘솔에서 웹앱을 만든 뒤 아래 값을 교체하세요.
+// 2) 값을 넣으면 홈페이지/관리자페이지가 Firestore 기반으로 실시간 연동됩니다.
+// 3) 값을 비워두면 같은 브라우저에서만 동작하는 데모 모드로 실행됩니다.
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+export const ADMIN_DEMO_PASSWORD = "admin1234";
