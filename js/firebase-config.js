0313f4b885bc38fad165860fb0a3bd74a7ae49b8
최병천 <soso100421@gmail.com>
2026-06-25 13:41:07 +0900

    관리자페이지 업데이트

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,5 @@
-// Firebase 설정 파일
-// 1) Firebase 콘솔에서 웹앱을 만든 뒤 아래 값을 교체하세요.
-// 2) 값을 넣으면 홈페이지/관리자페이지가 Firestore 기반으로 실시간 연동됩니다.
-// 3) 값을 비워두면 같은 브라우저에서만 동작하는 데모 모드로 실행됩니다.
+// Firebase를 사용할 경우 아래 값을 본인 프로젝트 값으로 교체하세요.
+// 비워두면 데모모드(localStorage)로 작동합니다.
 export const firebaseConfig = {
   apiKey: "AIzaSyDMn3t5QHpOSVqdAXsf8Bbh5TwXPqYIfpg",
   authDomain: "waterbridge-lms.firebaseapp.com",
@@ -11,5 +9,3 @@ export const firebaseConfig = {
   appId: "1:224313355988:web:886787fb9402df4f8e9541",
   measurementId: "G-XKFTYDRKXT"
 };
-
-export const ADMIN_DEMO_PASSWORD = "admin1234";
